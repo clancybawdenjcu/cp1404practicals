5aadba89b95e9ae26ec8b74bf7b3c82b9af1a6f6
clancybawdenjcu <clancy.bawden@my.jcu.edu.au>
2020-08-26 20:53:17 +1000

    Fix boundary conditions.

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -7,9 +7,9 @@ score = float(input("Enter score: "))
 while score < 0 or score > 100:
     print("Invalid score")
     score = float(input("Enter score: "))
-if score > 90:
+if score >= 90:
     print("Excellent")
-elif score > 50:
+elif score >= 50:
     print("Passable")
 else:
     print("Bad")
