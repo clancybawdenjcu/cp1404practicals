9e05dd930cea9787b9fbd1de6a202fe828a033c4
clancybawdenjcu <clancy.bawden@my.jcu.edu.au>
2020-08-26 20:45:57 +1000

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,7 +1,21 @@
 PASSWORD_MIN_LENGTH = 6
 
-password = input("Password: ")
-while len(password) < PASSWORD_MIN_LENGTH:
-    print(f"Password must be at least {PASSWORD_MIN_LENGTH} characters long.")
+
+def main():
+    password = get_password()
+    print_masked_password(password)
+
+
+def get_password():
     password = input("Password: ")
-print("*" * len(password))
+    while len(password) < PASSWORD_MIN_LENGTH:
+        print(f"Password must be at least {PASSWORD_MIN_LENGTH} characters long.")
+        password = input("Password: ")
+    return password
+
+
+def print_masked_password(password):
+    print("*" * len(password))
+
+
+main()
