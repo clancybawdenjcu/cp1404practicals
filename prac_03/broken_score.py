6235404f95c12b25e86fb9229d307c3e24b25c61
clancybawdenjcu <clancy.bawden@my.jcu.edu.au>
2020-08-26 21:51:26 +1000

    Update broken score program to use a function.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -3,13 +3,27 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
-    score = float(input("Enter score: "))
-if score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+
+def main():
+    # score = float(input("Enter score: "))
+    # while score < 0 or score > 100:
+    #     print("Invalid score")
+    #     score = float(input("Enter score: "))
+    score = random.randint(0, 100)
+    result = get_result(score)
+    print(f"Score: {score:>10}.")
+    print(f"Result: {result:>9}.")
+
+
+def get_result(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
