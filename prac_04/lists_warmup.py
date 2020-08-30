c468134d3680f89bf7d91f85e081cf85ec1a32f3
clancybawdenjcu <clancy.bawden@my.jcu.edu.au>
2020-08-30 20:35:31 +1000

    Add lists warmup file.

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,27 @@
+"""
+1. 3
+2. 2
+3. 1
+4. [3, 1, 4, 1, 5, 9]
+5. [1]
+6. True
+7. False
+8. False
+9. [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# 1.
+numbers[0] = "ten"
+
+# 2.
+numbers[-1] = 1
+
+# 3.
+new_numbers = numbers[2:]
+
+# 4.
+9 in numbers
+
+print(new_numbers)
