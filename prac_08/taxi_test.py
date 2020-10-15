02c3a33aa4e990804712e8db8d8999eacbf0e978
clancybawdenjcu <clancy.bawden@my.jcu.edu.au>
2020-10-15 12:04:10 +1000

    Finish prac_08 - UnreliableCar

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -3,7 +3,7 @@ from prac_08.taxi import Taxi
 
 def main():
     """Demo test code to learn inheritance."""
-    prius_1 = Taxi("Prius 1", 100, 1.23)
+    prius_1 = Taxi("Prius 1", 100)
     prius_1.drive(40)
     print(prius_1)
     prius_1.start_fare()
---
+++ prac_08/unreliable_car.py
@@ -0,0 +1,32 @@
+"""
+CP1404/CP5632 Practical
+Car class
+"""
+from prac_08.car import Car
+from random import randint
+
+
+class UnreliableCar(Car):
+    """Specialised version of a Car."""
+
+    def __init__(self, name, fuel, reliability):
+        """Initialise an UnreliableCar instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def __str__(self):
+        """Return a string like a Car but with current fare distance."""
+        return f"{(super().__str__())}, Reliability: {self.reliability}"
+
+    def start_fare(self):
+        pass
+
+    def drive(self, distance):
+        """Drive like parent Car but use use reliability value to determine if drive succeeds or fails."""
+        if randint(0, 100) <= self.reliability:
+            # print("Success")
+            distance_driven = super().drive(distance)
+        else:
+            # print("Fail")
+            distance_driven = 0
+        return distance_driven
